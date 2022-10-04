5ce0a8d2a665c22df525e0d8816c8dd6767dae31
Artem <artyom.getman1996@gmail.com>
2022-10-04 17:00:32 +1000

    add solution 283

--- easy/283. Move Zeroes.go
+++ easy/283. Move Zeroes.go
@@ -1,3 +1,4 @@
+// 1 Solution
 func moveZeroes(nums []int) {
 	zeroes := []int{}
 	numbers := []int{}
@@ -12,5 +13,21 @@ func moveZeroes(nums []int) {
 
 	for i, k := range append(numbers, zeroes...) {
 		nums[i] = k
+	}
+}
+
+// 2 Solution
+func moveZeroes(nums []int) {
+	lastZero := 0
+
+	for _, k := range nums {
+		if k != 0 {
+			nums[lastZero] = k
+			lastZero++
+		}
+	}
+
+	for ; lastZero < len(nums); lastZero++ {
+		nums[lastZero] = 0
 	}
 }
