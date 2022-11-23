74a74367fbf7db27b82b40bc777347e3764285bc
artem <artyom.getman1996@gmail.com>
2022-11-23 17:29:29 +1000

    add: 617

---
+++ easy/617. Merge Two Binary Trees.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	if root1 == nil && root2 == nil {
+		return nil
+	}
+	result := &TreeNode{}
+
+	// Проверки на наличие дочерних узлов
+	if root1 != nil && root2 == nil {
+		result.Val = root1.Val
+		result.Left = mergeTrees(root1.Left, nil)
+		result.Right = mergeTrees(root1.Right, nil)
+	} else if root1 == nil && root2 != nil {
+		result.Val = root2.Val
+		result.Left = mergeTrees(nil, root2.Left)
+		result.Right = mergeTrees(nil, root2.Right)
+	} else if root1 != nil && root2 != nil {
+		result.Val = root1.Val + root2.Val
+		result.Left = mergeTrees(root1.Left, root2.Left)
+		result.Right = mergeTrees(root1.Right, root2.Right)
+	}
+
+	return result
+}
